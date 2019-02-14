187bac5a5ec7e84c99ee362568035f10bbbe5210
Sawooop <jlang@stu.shaker.org>
2019-02-13 19:30:48 -0500

    ahhhh

---
+++ Week02/ClassesIntro/ClassesIntro/Resturaunt.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesIntro
+{
+    class Resturaunt
+    {
+        public string name;
+        public int NumberOfResturaunts;
+        public string TypeOfFood;
+        public int rating;
+        public void ResturauntOne()
+        {
+
+        }
+        public void ResturauntTwo()
+        {
+
+        }
+    }
+}
