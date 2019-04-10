288a5b9a7c2693510e0eda2c88b600c4b880d6fb
Sawooop <jlang@stu.shaker.org>
2019-04-10 19:25:46 -0400

    OHHHHHHHHHHHH BOYYYYYYYYYYYY lotta work to do

---
+++ JacksonLang'sChakraAligningProject/JacksonLang'sChakraAligningProject/BaseSearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JacksonLang_sChakraAligningProject
+{
+    class BaseSearch
+    {
+        private string Search;
+        public BaseSearch(string sarch)
+        {
+            Search = sarch;
+        }
+        public void WikipediaPage()
+        {
+
+            Search.Replace(" ", "_");
+            System.Diagnostics.Process.Start("https://en.wikipedia.org/wiki/" + Search);
+            //analyze wiki page for keywords
+            //make a few base classes (people, animals, objects, etc) based on the keywords
+            //create a summary of the search in a window that appears alongside the wiki page
+        }
+        public void Question()
+        {
+            //search google or maybe look up wiki for keywords and return a window that rooughly answers the question
+        }
+    }
+}
---
+++ JacksonLang'sChakraAligningProject/JacksonLang'sChakraAligningProject/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JacksonLang_sChakraAligningProject
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            string search = textBox.Text;
+            BaseSearch test = new BaseSearch(search);
+            if (search.IndexOf('?') > -1)
+            {
+                test.Question();
+            }
+            else
+            {
+                test.WikipediaPage();
+            }
+            textBox.Clear();
+        }
+    }
+        
+}
